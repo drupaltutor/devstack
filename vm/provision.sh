8d4e328d5ad3ae2fb2d49cdf4cb0cbecfc95c12d
Wayne Eaker <wayne@zengenuity.com>
2016-03-16 11:00:07 -0400

    Restart services after custom files are added.

--- vm/provision.sh
+++ vm/provision.sh
@@ -8,16 +8,19 @@ if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 if [ -d /vagrant-config/apache2/sites-enabled ]; then
   rm -rf /etc/apache2/sites-enabled
   ln -sv /vagrant-config/apache2/sites-enabled /etc/apache2/sites-enabled
+  /usr/sbin/apache2ctl restart
 fi
 
 # MySQL Custom
 if [ -f /vagrant-config/mysql/custom.cnf ]; then
   ln -sv /vagrant-config/mysql/custom.cnf /etc/mysql/conf.d/user.cnf
+  /usr/sbin/service mysql restart
 fi
 
 # PHP Custom
 if [ -f /vagrant-config/php/apache2/custom.ini ]; then
   ln -sv /vagrant-config/php/apache2/custom.ini /etc/php/7.0/apache2/conf.d/99-user.ini
+  /usr/sbin/apache2ctl restart
 fi
 if [ -f /vagrant-config/php/cli/custom.ini ]; then
   ln -sv /vagrant-config/php/cli/custom.ini /etc/php/7.0/cli/conf.d/99-user.ini
