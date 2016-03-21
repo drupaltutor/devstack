aecd48aa7d0d82e6e1f6f362237458e971ff5e9d
Wayne Eaker <wayne@zengenuity.com>
2016-03-21 11:42:29 -0400

    Add git configuration to provision script.

--- vm/provision.sh
+++ vm/provision.sh
@@ -25,3 +25,9 @@ fi
 if [ -f /vagrant-config/php/cli/custom.ini ]; then
   ln -sv /vagrant-config/php/cli/custom.ini /etc/php/7.0/cli/conf.d/99-user.ini
 fi
+
+# Git Configuration
+GIT_EMAIL=$1
+GIT_NAME=$2
+su - vagrant -c "git config --global user.email '$GIT_EMAIL'"
+su - vagrant -c "git config --global user.name '$GIT_NAME'"
